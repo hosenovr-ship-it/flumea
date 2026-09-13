8d89ddbdc54579cbbd0187754436412270b728ad
hosenovr-ship-it <hosenovr@gmail.com>
2026-09-13 11:40:14 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,77 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'login_screen.dart';
 
 void main() {
   runApp(const FlumeaApp());
+}
+
+class FlumeaApp extends StatelessWidget {
+  const FlumeaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FLUMEA',
+      home: const WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // إخفاء الوقت والواي فاي والبطارية أثناء شاشة الترحيب
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.immersiveSticky,
+    );
+
+    Timer(const Duration(seconds: 2), () {
+      if (!mounted) return;
+
+      // إعادة واجهة النظام قبل الانتقال لتسجيل الدخول
+      SystemChrome.setEnabledSystemUIMode(
+        SystemUiMode.edgeToEdge,
+      );
+
+      Navigator.of(context).pushReplacement(
+        PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) {
+            return const LoginScreen();
+          },
+          transitionsBuilder:
+              (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+          transitionDuration: const Duration(milliseconds: 600),
+        ),
+      );
+    });
   }
 
-  class FlumeaApp extends StatelessWidget {
-    const FlumeaApp({super.key});
-
-      @override
-        Widget build(BuildContext context) {
-            return MaterialApp(
-                  debugShowCheckedModeBanner: false,
-                        title: 'FLUMEA',
-                              home: const WelcomeScreen(),
-                                  );
-                                    }
-                                    }
-
-                                    class WelcomeScreen extends StatefulWidget {
-                                      const WelcomeScreen({super.key});
-
-                                        @override
-                                          State<WelcomeScreen> createState() => _WelcomeScreenState();
-                                          }
-
-                                          class _WelcomeScreenState extends State<WelcomeScreen> {
-                                            @override
-                                              void initState() {
-                                                  super.initState();
-
-                                                      Timer(const Duration(seconds: 2), () {
-                                                            if (!mounted) return;
-
-                                                                  Navigator.of(context).pushReplacement(
-                                                                          PageRouteBuilder(
-                                                                                    pageBuilder: (context, animation, secondaryAnimation) {
-                                                                                                return const LoginScreen();
-                                                                                                          },
-                                                                                                                    transitionsBuilder:
-                                                                                                                                  (context, animation, secondaryAnimation, child) {
-                                                                                                                                              return FadeTransition(
-                                                                                                                                                            opacity: animation,
-                                                                                                                                                                          child: child,
-                                                                                                                                                                                      );
-                                                                                                                                                                                                },
-                                                                                                                                                                                                          transitionDuration: const Duration(milliseconds: 600),
-                                                                                                                                                                                                                  ),
-                                                                                                                                                                                                                        );
-                                                                                                                                                                                                                            });
-                                                                                                                                                                                                                              }
-
-                                                                                                                                                                                                                                @override
-                                                                                                                                                                                                                                  Widget build(BuildContext context) {
-                                                                                                                                                                                                                                      return Scaffold(
-                                                                                                                                                                                                                                            body: SizedBox.expand(
-                                                                                                                                                                                                                                                    child: Image.asset(
-                                                                                                                                                                                                                                                              'assets/icons/images/welcome_background.png',
-                                                                                                                                                                                                                                                                        fit: BoxFit.cover,
-                                                                                                                                                                                                                                                                                ),
-                                                                                                                                                                                                                                                                                      ),
-                                                                                                                                                                                                                                                                                          );
-                                                                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                                                            }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox.expand(
+        child: Image.asset(
+          'welcome_background.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
