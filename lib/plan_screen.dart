d95cb3a9f97cf1d4d678311c7fba30b17df4badc
hosenovr-ship-it <hosenovr@gmail.com>
2026-09-18 16:29:42 +0300

    Update plan_screen.dart

--- lib/plan_screen.dart
+++ lib/plan_screen.dart
@@ -695,10 +695,14 @@ class PlanScreen extends StatelessWidget {
             color: isCompleted
                 ? const Color(0xFF20C7B7)
                 : const Color(0xFFB8C1CC),
-                ],
-  ),
+                ),
+],
+),
+),
 );
-          }
+},
+);
+  }
 
   // =========================================================
   // WEEKLY GOALS
