edc086a1c3a2673628b2969772bc7c9e4d573656
hosenovr-ship-it <hosenovr@gmail.com>
2026-09-25 10:41:49 +0300

    Add files via upload

--- lib/theme_controller.dart
+++ lib/theme_controller.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+class FlumeaThemeController {
+  static final ValueNotifier<ThemeMode> mode =
+      ValueNotifier<ThemeMode>(ThemeMode.light);
+
+  static Future<void> load() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDark = prefs.getBool('flumea_dark_mode') ?? false;
+    mode.value = isDark ? ThemeMode.dark : ThemeMode.light;
+  }
+
+  static Future<void> setMode(ThemeMode newMode) async {
+    mode.value = newMode;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('flumea_dark_mode', newMode == ThemeMode.dark);
+  }
+}
